2e4f358228b234ae5c517a300416fca9731711e4
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-17 00:23:00 +0200

    Initial commit

---
+++ ExileParty/Hubs/MainHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ExileParty.Hubs
+{
+    [EnableCors("AllowAll")]
+    public class MainHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "joined");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "left");
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("SendMessage", Context.User.Identity.Name, message);
+        }
+    }
+}
