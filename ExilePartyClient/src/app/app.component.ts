50ec9df9e7346ea084d423b7c6b4e44612a92445
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-17 00:25:17 +0200

    Added initial electron-client

---
+++ ExilePartyClient/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ElectronService } from './shared/providers/electron.service';
+import { TranslateService } from '@ngx-translate/core';
+import { AppConfig } from './app.config';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(public electronService: ElectronService,
+    private translate: TranslateService) {
+
+    translate.setDefaultLang('en');
+    console.log('AppConfig', AppConfig);
+
+    if (electronService.isElectron()) {
+      console.log('Mode electron');
+      console.log('Electron ipcRenderer', electronService.ipcRenderer);
+      console.log('NodeJS childProcess', electronService.childProcess);
+    } else {
+      console.log('Mode web');
+    }
+  }
+}
