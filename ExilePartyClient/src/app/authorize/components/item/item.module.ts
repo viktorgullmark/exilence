4bc3c0c53682736b179257901679859d77e7b325
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-23 00:12:41 +0200

    Started working on tooltip

--- ExilePartyClient/src/app/authorize/components/item/item.module.ts
+++ ExilePartyClient/src/app/authorize/components/item/item.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { ItemComponent } from './item.component';
 import { ItemSocketsComponent } from './item-sockets/item-sockets.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { ItemTooltipComponent } from './item-tooltip/item-tooltip.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [ItemComponent, ItemSocketsComponent],
-  exports: [ItemComponent, ItemSocketsComponent]
+  declarations: [ItemComponent, ItemSocketsComponent, ItemTooltipComponent],
+  exports: [ItemComponent, ItemSocketsComponent, ItemTooltipComponent]
 })
 export class ItemModule { }
