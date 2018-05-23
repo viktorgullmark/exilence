00309bd319d03309e2375070e6621f154999bbcf
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2018-05-23 14:23:46 +0200

    Basic positioning of item-tooltips

--- ExilePartyClient/src/app/authorize/components/item/item.component.ts
+++ ExilePartyClient/src/app/authorize/components/item/item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChildren, ViewChild } from '@angular/core';
 import { Item } from '../../../shared/interfaces/item.interface';
+import { ItemTooltipComponent } from './item-tooltip/item-tooltip.component';
 
 @Component({
   selector: 'app-item',
@@ -10,9 +11,30 @@ export class ItemComponent implements OnInit {
   @Input() item: Item;
   @Input() wide = false;
   @Input() weaponSwap = false;
-  constructor() { }
+  @ViewChild('tooltip') tooltip: ElementRef;
+  top: number;
+  left: number;
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
     console.log('item:', this.item);
   }
+
+  positionTooltip() {
+    const element = this.el.nativeElement;
+
+    const rect = this.tooltip.nativeElement.getBoundingClientRect();
+    const elementRect = element.getBoundingClientRect();
+
+    const overflowTop = rect.top - 65;
+    const overflowLeft = rect.left - 280;
+
+    if (overflowTop < 0) {
+      this.top = this.tooltip.nativeElement.offsetTop - overflowTop;
+    }
+    if (overflowLeft < 0) {
+      this.left = this.tooltip.nativeElement.offsetLeft - overflowLeft;
+    }
+  }
+
 }
