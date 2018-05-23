56b2f8a1bbbae10b5b17bcecdfacc5edc33b48a6
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-24 00:56:58 +0200

    Work in progress: Item tooltip and character-preview

--- ExilePartyClient/src/app/authorize/components/char-equipment/char-equipment.module.ts
+++ ExilePartyClient/src/app/authorize/components/char-equipment/char-equipment.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharEquipmentComponent } from './char-equipment.component';
 import { ItemModule } from '../item/item.module';
+import { EquipmentSlotComponent } from './equipment-slot/equipment-slot.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ItemModule
   ],
-  declarations: [CharEquipmentComponent],
-  exports: [CharEquipmentComponent]
+  declarations: [CharEquipmentComponent, EquipmentSlotComponent],
+  exports: [CharEquipmentComponent, EquipmentSlotComponent]
 })
 export class CharEquipmentModule { }
---
+++ ExilePartyClient/src/app/authorize/components/item/item-tooltip/item-tooltip-content/item-tooltip-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-tooltip-content',
+  templateUrl: './item-tooltip-content.component.html',
+  styleUrls: ['./item-tooltip-content.component.scss']
+})
+export class ItemTooltipContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
