e25d0eb94146b25cf65661baa6c91b4456ede0aa
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2018-05-23 15:13:55 +0200

    Extended tooltip repositioning

--- ExilePartyClient/src/app/authorize/components/item/item-tooltip/item-tooltip.component.ts
+++ ExilePartyClient/src/app/authorize/components/item/item-tooltip/item-tooltip.component.ts
@@ -9,6 +9,8 @@ import { Item } from '../../../../shared/interfaces/item.interface';
 export class ItemTooltipComponent implements OnInit {
   @Input() item: Item;
   nativeElement: HTMLElement;
+  top: number;
+  left: number;
   constructor(private el: ElementRef) {
     this.nativeElement = el.nativeElement;
   }
@@ -16,4 +18,31 @@ export class ItemTooltipComponent implements OnInit {
   ngOnInit() {
   }
 
+  reposition(event, host: ElementRef) {
+    const mousePosX = event.clientX;
+    const mousePosY = event.clientY;
+    const element = host.nativeElement;
+
+    const rect = this.el.nativeElement.getBoundingClientRect();
+    const elementRect = element.getBoundingClientRect();
+    const margin = 25;
+
+    const overflowTop = rect.top - (54 + margin);
+    const overflowLeft = rect.left - (265 + margin);
+    const overflowRight = window.innerWidth - rect.right;
+    const overflowBottom = window.innerHeight - rect.bottom;
+
+    if (overflowTop < 0) {
+      this.top = this.el.nativeElement.offsetTop - overflowTop;
+    }
+    if (overflowLeft < 0) {
+      this.left = this.el.nativeElement.offsetLeft - overflowLeft;
+    }
+    if (overflowBottom < 0) {
+      this.top = this.el.nativeElement.offsetTop + overflowBottom - margin;
+    }
+    if (overflowRight < 0) {
+      this.left = this.el.nativeElement.offsetLeft + overflowRight - margin;
+    }
+  }
 }
