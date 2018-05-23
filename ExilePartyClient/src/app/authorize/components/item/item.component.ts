9be67e94874af6be208e633110f7e7861d679f1f
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2018-05-23 15:13:55 +0200

    Extended tooltip repositioning

--- ExilePartyClient/src/app/authorize/components/item/item.component.ts
+++ ExilePartyClient/src/app/authorize/components/item/item.component.ts
@@ -11,30 +11,9 @@ export class ItemComponent implements OnInit {
   @Input() item: Item;
   @Input() wide = false;
   @Input() weaponSwap = false;
-  @ViewChild('tooltip') tooltip: ElementRef;
-  top: number;
-  left: number;
-  constructor(private el: ElementRef) { }
+  @ViewChild('tooltip') tooltip: ItemTooltipComponent;
+  constructor(public el: ElementRef) { }
 
   ngOnInit() {
-    console.log('item:', this.item);
   }
-
-  positionTooltip() {
-    const element = this.el.nativeElement;
-
-    const rect = this.tooltip.nativeElement.getBoundingClientRect();
-    const elementRect = element.getBoundingClientRect();
-
-    const overflowTop = rect.top - 65;
-    const overflowLeft = rect.left - 280;
-
-    if (overflowTop < 0) {
-      this.top = this.tooltip.nativeElement.offsetTop - overflowTop;
-    }
-    if (overflowLeft < 0) {
-      this.left = this.tooltip.nativeElement.offsetLeft - overflowLeft;
-    }
-  }
-
 }
