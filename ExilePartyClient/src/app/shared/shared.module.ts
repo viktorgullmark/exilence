cd3138e3bea219ef5a00c0f94aa674b61098d036
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2018-05-25 14:13:43 +0200

    Added loading-spinner

--- ExilePartyClient/src/app/shared/shared.module.ts
+++ ExilePartyClient/src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   imports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    MatProgressSpinnerModule
   ]
 })
 
