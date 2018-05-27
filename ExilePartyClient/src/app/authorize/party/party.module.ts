9d3ddb79db650dc97729ba6cc4c499b403bbfc08
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-27 23:22:45 +0200

    Style-changes

--- ExilePartyClient/src/app/authorize/party/party.module.ts
+++ ExilePartyClient/src/app/authorize/party/party.module.ts
@@ -4,10 +4,11 @@ import { SharedModule } from '../../shared/shared.module';
 import { PartyComponent } from './party.component';
 import { PlayerListModule } from './player-list/player-list.module';
 import { CharProfileModule } from '../components/char-profile/char-profile.module';
-
+import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   imports: [
     SharedModule,
+    MatDividerModule,
     PlayerListModule,
     CharProfileModule
   ],
