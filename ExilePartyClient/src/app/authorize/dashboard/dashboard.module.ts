6ad61c71a97f21b66971175b4658a1b35bd0c79f
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-27 23:22:45 +0200

    Style-changes

--- ExilePartyClient/src/app/authorize/dashboard/dashboard.module.ts
+++ ExilePartyClient/src/app/authorize/dashboard/dashboard.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../../shared/shared.module';
 import { CharProfileModule } from '../components/char-profile/char-profile.module';
+import { MatDividerModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    MatDividerModule
   ],
   declarations: [DashboardComponent]
 })
