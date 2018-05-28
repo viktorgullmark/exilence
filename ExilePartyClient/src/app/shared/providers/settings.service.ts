949e4d6322a6f8deca274ce05031b39bfa6561eb
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-05-28 23:20:02 +0200

    Added settings-service with working account-info storage

---
+++ ExilePartyClient/src/app/shared/providers/settings.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AccountService } from './account.service';
+import { PartyService } from './party.service';
+import { Player } from '../interfaces/player.interface';
+import { ElectronService } from './electron.service';
+
+@Injectable()
+export class SettingsService {
+  constructor(private electronService: ElectronService) {
+  }
+
+  set(key: string, object: any) {
+    this.electronService.settings.set(key, object);
+  }
+  get(key: string) {
+    return this.electronService.settings.get(key);
+  }
+}
