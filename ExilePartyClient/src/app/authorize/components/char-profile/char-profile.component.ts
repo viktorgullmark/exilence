df7bcfc686422cfcacd8171e8a586c854752b345
Petter Annerwall <petterannerwall@gmail.com>
2018-05-29 17:04:44 +0200

    Work in progress calculate mods, needs refactoring

--- ExilePartyClient/src/app/authorize/components/char-profile/char-profile.component.ts
+++ ExilePartyClient/src/app/authorize/components/char-profile/char-profile.component.ts
@@ -13,12 +13,62 @@ import { SessionService } from '../../../shared/providers/session.service';
 })
 export class CharProfileComponent implements OnInit {
   player: Player;
+
+  mods: any[] = [];
+
+  twoStoneRegex = '\\+(.*)\\%\\sto (Fire|Cold|Lightning) and (Fire|Cold|Lightning) Resistance';
+  singleRegex = '\\+(.*)\\%\\sto ((?:Fire|Cold|Lightning) Resistance)';
+
   constructor(private partyService: PartyService, private sessionService: SessionService,
     private externalService: ExternalService, private router: Router) { }
 
   ngOnInit() {
     this.partyService.selectedPlayer.subscribe(res => {
       this.player = res;
+      this.calculateResistances(res);
     });
+  }
+
+  calculateResistances(player: Player) {
+
+    player.character.items.forEach(item => {
+
+      let mods = [];
+      if (item.craftedMods) {
+        mods = mods.concat(item.craftedMods);
+      }
+      if (item.explicitMods) {
+        mods = mods.concat(item.explicitMods);
+      }
+      if (item.implicitMods) {
+        mods = mods.concat(item.implicitMods);
+      }
+      if (item.enchantMods) {
+        mods = mods.concat(item.enchantMods);
+      }
+
+      mods.forEach((mod: string) => {
+
+        const singleMatch = mod.match(new RegExp(this.singleRegex));
+        if (singleMatch) {
+          this.modifyValues(singleMatch[1], singleMatch[2]);
+        }
+
+        const twoStoneMatch = mod.match(new RegExp(this.twoStoneRegex));
+        if (twoStoneMatch) {
+          this.modifyValues(twoStoneMatch[1], twoStoneMatch[2] + ' Resistance');
+          this.modifyValues(twoStoneMatch[1], twoStoneMatch[3] + ' Resistance');
+        }
+      });
+    });
+    console.log(this.mods);
+  }
+
+  modifyValues(value, res) {
+    if (this.mods[res]) {
+      this.mods[res] += parseInt(value, 10);
+    } else {
+      this.mods[res] = parseInt(value, 10);
+    }
   }
 }
