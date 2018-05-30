ec5e8bfcde82d32c1615c51aae422b3d3cb5a985
Petter Annerwall <petterannerwall@gmail.com>
2018-05-30 14:54:45 +0200

    Added recent parties

--- ExilePartyClient/src/app/authorize/dashboard/dashboard.module.ts
+++ ExilePartyClient/src/app/authorize/dashboard/dashboard.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { DashboardComponent } from './dashboard.component';
-import { SharedModule } from '../../shared/shared.module';
-import { CharProfileModule } from '../components/char-profile/char-profile.module';
 import { MatDividerModule } from '@angular/material';
+
+import { SharedModule } from '../../shared/shared.module';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
