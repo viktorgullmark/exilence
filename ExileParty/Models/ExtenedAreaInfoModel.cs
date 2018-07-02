79a91ed8f9083ca7c056aae9f98a9c9b9baae600
Petter <petterannerwall@gmail.com>
2018-07-02 23:55:20 +0200

    Added tier to event area info

--- ExileParty/Models/ExtenedAreaInfoModel.cs
+++ ExileParty/Models/ExtenedAreaInfoModel.cs
@@ -27,6 +27,7 @@ namespace ExileParty.Models
     {
         public int? Act { get; set; }
         public int? Level { get; set; }
+        public int? Tier { get; set; }
         public bool Town { get; set; }
         public bool Trial { get; set; }
         public bool Waypoint { get; set; }
