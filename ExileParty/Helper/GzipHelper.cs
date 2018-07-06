17bcd299f5d063af1f1d0f3da641a47d953242fb
Petter Annerwall <petterannerwall@gmail.com>
2018-07-06 15:03:33 +0200

    Started working on compression

---
+++ ExileParty/Helper/GzipHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExileParty.Helper
+{
+    public static class GzipHelper
+    {
+        public static string Zip(string input)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+
+            using (var outputStream = new MemoryStream())
+            {
+                using (var gZipStream = new GZipStream(outputStream, CompressionMode.Compress))
+                    gZipStream.Write(inputBytes, 0, inputBytes.Length);
+
+                var outputBytes = outputStream.ToArray();
+
+                var output = Encoding.UTF8.GetString(outputBytes);
+
+                return output;
+            }
+        }
+
+        public static string Unzip(string input)
+        {
+            string output = null;
+
+            byte[] inputBytes = Convert.FromBase64String(input);
+
+            using (var inputStream = new MemoryStream(inputBytes))
+            using (var gZipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+            using (var streamReader = new StreamReader(gZipStream))
+            {
+                output = streamReader.ReadToEnd();
+
+            }
+
+            return output;
+        }
+    }
+}
