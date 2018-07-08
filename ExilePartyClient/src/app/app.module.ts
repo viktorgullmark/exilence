75179a6e10ad1f1e8e6ca38e0232b0a1b55384d3
Viktor Gullmark <viktor.gullmark@hotmail.com>
2018-07-08 16:14:44 +0200

    Moved disconnectedmodule

--- ExilePartyClient/src/app/app.module.ts
+++ ExilePartyClient/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { LogService } from './shared/providers/log.service';
 import { NinjaService } from './shared/providers/ninja.service';
 import { SessionService } from './shared/providers/session.service';
 import { SettingsService } from './shared/providers/settings.service';
+import { DisconnectedModule } from './disconnected/disconnected.module';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
