eb372640b164149f62658bf0d91746738810b391
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2018-07-09 14:53:26 +0200

    Added link to player-profile

--- ExilePartyClient/src/app/authorize/components/char-profile/char-equipment/char-equipment.component.ts
+++ ExilePartyClient/src/app/authorize/components/char-profile/char-equipment/char-equipment.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Item } from '../../../../shared/interfaces/item.interface';
 import * as data from './equipment-slots';
+import { ElectronService } from '../../../../shared/providers/electron.service';
+import { Player } from '../../../../shared/interfaces/player.interface';
+import { PartyService } from '../../../../shared/providers/party.service';
 
 @Component({
   selector: 'app-char-equipment',
@@ -12,10 +15,14 @@ export class CharEquipmentComponent implements OnInit {
   @Input() items: Item[];
   equipment = data.equipmentSlots;
   flasks = data.flaskSlots;
-  constructor() {
+  selectedPlayer: Player;
+  constructor(private electronService: ElectronService, private partyService: PartyService) {
   }
 
   ngOnInit() {
+    this.partyService.selectedPlayer.subscribe(res => {
+      this.selectedPlayer = res;
+    });
   }
 
   getItemByType(type: string) {
@@ -23,5 +30,14 @@ export class CharEquipmentComponent implements OnInit {
   }
   getFlaskByIndex(index: number) {
     return this.items.find(x => x.inventoryId === 'Flask' && x.x === index);
+  }
+  openLink(link: string) {
+    this.electronService.shell.openExternal(link);
+  }
+  goToProfile() {
+    this.openLink('https://www.pathofexile.com/account/view-profile/'
+      + this.selectedPlayer.account
+      + '/characters?characterName='
+      + this.selectedPlayer.character.name);
   }
 }
