a3a8434c060efe1aec34eefd826e08e6d4d1cc99
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2018-07-10 14:46:19 +0200

    Layout for keybindings

--- ExilePartyClient/src/app/authorize/settings/settings.component.ts
+++ ExilePartyClient/src/app/authorize/settings/settings.component.ts
@@ -5,6 +5,7 @@ import { AnalyticsService } from '../../shared/providers/analytics.service';
 import { StashtabListComponent } from '../components/stashtab-list/stashtab-list.component';
 import { ElectronService } from '../../shared/providers/electron.service';
 import { SettingsService } from '../../shared/providers/settings.service';
+import { Keys } from '../../shared/interfaces/key.interface';
 
 @Component({
   selector: 'app-settings',
@@ -15,6 +16,22 @@ export class SettingsComponent implements OnInit {
   form: FormGroup;
   selectedIndex = 0;
   alwaysOnTop = false;
+
+  // temporary arrays
+  modifierKeys = [
+    { id: 1, name: 'Shift', code: Keys.Shift },
+    { id: 2, name: 'Ctrl', code: Keys.Ctrl }
+  ];
+  triggerKeys = [
+    { id: 1, name: 'F1', code: Keys.F1 },
+    { id: 2, name: 'F2', code: Keys.F2 }
+  ];
+  keybinds = [
+    { name: 'Send net worth summary to party', triggerKeyId: 1, modifierKeyId: 1 },
+    { name: 'Test 2', triggerKeyId: 1, modifierKeyId: 2 },
+    { name: 'Test 3', triggerKeyId: 2, modifierKeyId: 1 },
+    { name: 'Test 4', triggerKeyId: 2, modifierKeyId: 2 }
+  ];
 
   @ViewChild('table') table: StashtabListComponent;
 
@@ -37,6 +54,14 @@ export class SettingsComponent implements OnInit {
     if (onTopSetting !== undefined) {
       this.alwaysOnTop = onTopSetting;
     }
+  }
+
+  resetKeybinds() {
+    // todo: reset keybinds
+  }
+
+  saveKeybinds() {
+    // todo: save keybinds
   }
 
   search() {
