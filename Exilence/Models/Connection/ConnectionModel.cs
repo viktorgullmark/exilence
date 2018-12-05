330cc5de37db52da2f860a301023e78084beb5cf
Petter Annerwall <petterannerwall@gmail.com>
2018-12-05 23:56:45 +0100

    Added missing serializeable

--- Exilence/Models/Connection/ConnectionModel.cs
+++ Exilence/Models/Connection/ConnectionModel.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Exilence.Models.Connection
 {
+    [Serializable]
     public class ConnectionModel
-    {
+    {   
         [Key]
         public string ConnectionId { get; set; }
         public string PartyName { get; set; }
