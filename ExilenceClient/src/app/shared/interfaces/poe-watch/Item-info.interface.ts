93de5b649ffd089acba1b954d5844fd73fc0eccd
Petter Annerwall <petterannerwall@gmail.com>
2018-12-19 19:57:41 +0100

    added poe-watch service

---
+++ ExilenceClient/src/app/shared/interfaces/poe-watch/Item-info.interface.ts
@@ -0,0 +1,16 @@
+export interface ItemInfo {
+  id: number;
+  name: string;
+  type: string;
+  frame: number;
+  tier?: any;
+  lvl?: any;
+  quality?: any;
+  corrupted?: any;
+  links?: any;
+  ilvl?: any;
+  variation?: any;
+  icon: string;
+  category: string;
+  group: string;
+}
---
+++ ExilenceClient/src/app/shared/interfaces/poe-watch/item-price.interface.ts
@@ -0,0 +1,11 @@
+export interface ItemPrice {
+  id: number;
+  mean: number;
+  median: number;
+  mode: number;
+  min: number;
+  max: number;
+  exalted: number;
+  count: number;
+  quantity: number;
+}
