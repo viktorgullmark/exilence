042f9a270d679a239bcfd6f8a6322693bf9cf072
Jake <valseria@pm.me>
2018-12-19 03:47:14 -0800

    Moved CompressionHelper.cs to the Encoding folder and added LZ4 encoding settings

--- Exilence/Encoding/CompressionHelper.cs
+++ Exilence/Encoding/CompressionHelper.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Exilence.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 using K4os.Compression.LZ4.Streams;
+using K4os.Hash.xxHash;
+using Exilence.Encoding;
 
 namespace Exilence.Helper
 {
-    public static class LZ4_CompressionHelper
+    public static class CompressionHelper
     {
         public static string Compress<T>(T input)
         {
-            
             var jsonString = JsonConvert.SerializeObject(input, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
 
-            var buffer = Encoding.UTF8.GetBytes(jsonString);
-
-            using (var outputStream = new MemoryStream())
+            //inputStream is the output of the jsonString object.
+            using (var inputStream = new MemoryStream())
             {
-                //var output = new LZ4EncoderStream(outputStream, )
+                var output = LZ4Stream.Encode(inputStream).ToString();
 
-                var outputBytes = outputStream.ToArray();
-
-                var base64String = Convert.ToBase64String(outputBytes);
-
-                return base64String;
+                return output;
             }
         }
 
@@ -38,11 +34,11 @@ namespace Exilence.Helper
         {
             string jsonString = null;
 
-
             byte[] inputBytes = Convert.FromBase64String(input);
 
             using (var inputStream = new MemoryStream(inputBytes))
-            using (var gZipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+
+
             using (var streamReader = new StreamReader(gZipStream))
             {
                 jsonString = streamReader.ReadToEnd();
---
+++ Exilence/Encoding/LZ4EncoderSettings.cs
@@ -0,0 +1,21 @@
+﻿using K4os.Compression.LZ4;
+using K4os.Compression.LZ4.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exilence.Encoding
+{
+    class LZ4EncoderSettings
+    {
+        long? ContentLength { get; set; } = null;
+        bool ChainBlocks { get; set; } = true;
+        int BlockSize { get; set; } = Mem.K64;
+        bool ContentChecksum => false;
+        bool BlockChecksum => false;
+        uint? Dictionary => null;
+        LZ4Level CompressionLevel { get; set; } = LZ4Level.L00_FAST;
+        int ExtraMemory { get; set; } = 0;
+    }
+}
