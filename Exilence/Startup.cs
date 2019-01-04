081d41b189d6875b53bdb141095f238fc72c2f15
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-01-04 01:14:04 +0100

    Temporarily disabled removal of ladder-index

--- Exilence/Startup.cs
+++ Exilence/Startup.cs
@@ -95,7 +95,7 @@ namespace Exilence
             app.UseHangfireServer(hangfireOpts);
             app.UseHangfireDashboard();
 
-            //BackgroundJob.Schedule<ILadderService>(ls => ls.UpdateLadders(), TimeSpan.FromMilliseconds(5000));
+            BackgroundJob.Schedule<ILadderService>(ls => ls.UpdateLadders(), TimeSpan.FromMilliseconds(5000));
 
             app.UseSignalR(routes =>
             {
