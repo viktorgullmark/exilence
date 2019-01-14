15954ab13e3baf5ec5f919676a5513b68d8e63b1
Jaanus <sellinjaanus@gmail.com>
2019-01-14 21:49:01 +0200

    Fix ips

--- ExilenceClient/src/environments/environment.prod.ts
+++ ExilenceClient/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
   production: true,
   environment: 'PROD',
   sentryDsn: 'https://0db02ec6abdd490d8e749491dcaf2a96@sentry.io/1370601',
-  url: 'http://116.203.0.114/'
+  url: 'http://116.203.0.113/'
 };
