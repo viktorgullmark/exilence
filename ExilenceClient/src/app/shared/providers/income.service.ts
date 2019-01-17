2bc307e84a631956259626e58691fec888d75365
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-01-17 16:26:47 +0100

    Fixed a bug where SSF players could not use the maptab

--- ExilenceClient/src/app/shared/providers/income.service.ts
+++ ExilenceClient/src/app/shared/providers/income.service.ts
@@ -216,6 +216,7 @@ export class IncomeService implements OnDestroy {
 
     const accountName = this.localPlayer.account;
     const league = this.localPlayer.character.league;
+    const tradeLeague = this.settingsService.get('account.tradeLeagueName');
 
     this.playerStashTabs = [];
     this.totalNetWorthItems = [];
@@ -245,7 +246,7 @@ export class IncomeService implements OnDestroy {
     }
 
     return Observable.forkJoin(
-      this.getPlayerPublicMaps(accountName, league, mapTab),
+      this.getPlayerPublicMaps(accountName, tradeLeague, mapTab),
       this.getPlayerStashTabs(accountName, league),
       this.pricingService.retrieveExternalPrices()
     ).do(() => {
