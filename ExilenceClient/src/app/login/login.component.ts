ab4599e5985c26f8289b77e141cea54a294a9e29
Petter Annerwall <petterannerwall@gmail.com>
2019-01-30 21:25:26 +0100

    Added auto-join of party

--- ExilenceClient/src/app/login/login.component.ts
+++ ExilenceClient/src/app/login/login.component.ts
@@ -121,6 +121,14 @@ export class LoginComponent implements OnInit, OnDestroy {
         this.mapService.previousInstanceServer = undefined;
         this.mapService.previousDate = undefined;
         this.mapService.currentArea = undefined;
+
+        if (!this.electronService.isElectron()) {
+            const partyName = window['partyName'];
+            if (partyName !== '' && partyName !== undefined && partyName != null) {
+                console.log('Joining party: ', partyName);
+                setTimeout(() => this.loadGroup(partyName), 1000);
+            }
+        }
     }
 
     checkPath() {
