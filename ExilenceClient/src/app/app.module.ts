ef712a8614d9a343c03d44b7991c1100746dd406
Petter Annerwall <petterannerwall@gmail.com>
2019-02-02 16:39:00 +0100

    Initian commit for state service

--- ExilenceClient/src/app/app.module.ts
+++ ExilenceClient/src/app/app.module.ts
@@ -36,6 +36,7 @@ import { NinjaService } from './shared/providers/ninja.service';
 import { SessionService } from './shared/providers/session.service';
 import { SettingsService } from './shared/providers/settings.service';
 import { PartyService } from './shared/providers/party.service';
+import { StateService } from './shared/providers/state.service';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -71,10 +72,11 @@ export function HttpLoaderFactory(http: HttpClient) {
       }
     }),
   ],
-  providers: [
+  providers: [,
     ExternalService,
     AnalyticsService,
     ElectronService,
+    StateService,
     LogService,
     CanActivateAuthorized,
     AccountService,
