aeb829cf8f0a57b76b15a3dfe17b1d9233b68df7
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-02-03 20:53:32 +0100

    First version of appsettings for consoleapp

--- LadderService/Program.cs
+++ LadderService/Program.cs
@@ -1,149 +1,165 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
 using Shared.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace LadderService
 {
     class Program
     {
-        private static RedisRepository _redisRepository;
-
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            _redisRepository = new RedisRepository();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddDistributedRedisCache(options =>
+                {
+                    options.Configuration = configuration.GetConnectionString("Redis");
+                    options.InstanceName = "Exilence:";
+                })
+                .BuildServiceProvider();
+
+            string myKey1 = configuration["Spectator:Key"];
+            Console.WriteLine(myKey1);
 
             Console.ReadKey();
         }
 
-        public async Task UpdateLadders()
-        {
-            var leagues = await _redisRepository.GetAllLeaguesLadders();
-            if (leagues != null)
-            {
-                var anyRunning = leagues.Any(t => t.Running);
-                var pendingLeague = leagues.OrderByDescending(t => t.Finished).LastOrDefault();
+        //public async Task UpdateLadders()
+        //{
+        //    var leagues = await _redisRepository.GetAllLeaguesLadders();
+        //    if (leagues != null)
+        //    {
+        //        var anyRunning = leagues.Any(t => t.Running);
+        //        var pendingLeague = leagues.OrderByDescending(t => t.Finished).LastOrDefault();
 
-                if (!anyRunning)
-                {
-                    if (pendingLeague != null)
-                    {
-                        if (pendingLeague.Finished < DateTime.Now.AddMinutes(-5))
-                        {
-                            await UpdateLadder(pendingLeague.Name);
-                        }
-                    }
-                }
-            }
-        }
+        //        if (!anyRunning)
+        //        {
+        //            if (pendingLeague != null)
+        //            {
+        //                if (pendingLeague.Finished < DateTime.Now.AddMinutes(-5))
+        //                {
+        //                    await UpdateLadder(pendingLeague.Name);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
 
-        private async Task UpdateLadder(string leagueName)
-        {
-            await _redisRepository.SetLeagueLadderRunning(leagueName);
+        //private async Task UpdateLadder(string leagueName)
+        //{
+        //    await _redisRepository.SetLeagueLadderRunning(leagueName);
 
-            var league = await _redisRepository.GetLeagueLadder(leagueName);
-            var oldLadder = league.Ladder;
-            var newLadder = new List<LadderPlayerModel>();
+        //    var league = await _redisRepository.GetLeagueLadder(leagueName);
+        //    var oldLadder = league.Ladder;
+        //    var newLadder = new List<LadderPlayerModel>();
 
-            var pages = Enumerable.Range(0, 25);
-            using (var rateGate = new RateGate(2, TimeSpan.FromSeconds(1)))
-            {
-                foreach (int page in pages)
-                {
-                    await rateGate.WaitToProceed();
-                    LadderApiResponse result = await FetchLadderApiPage(leagueName, page);
-                    if (result != null)
-                    {
-                        var LadderPlayerList = result.Entries.Select(t => new LadderPlayerModel()
-                        {
-                            Name = t.Character.Name,
-                            Level = t.Character.Level,
-                            Online = t.Online,
-                            Dead = t.Dead,
-                            Account = t.Account.Name,
-                            Experience = t.Character.Experience,
-                            ExperiencePerHour = 0,
-                            Rank = new LadderPlayerRankModel()
-                            {
-                                Overall = t.Rank
-                            },
-                            Depth = new LadderPlayerDepthModel()
-                            {
-                                Solo = t.Character.Depth != null ? t.Character.Depth.Solo : 0,
-                                Group = t.Character.Depth != null ? t.Character.Depth.@default : 0
-                            },
-                            Twitch = t.Account.Twitch?.Name,
-                            Class = t.Character.Class,
-                            Updated = DateTime.Now
-                        }).ToList();
-                        // Convert result to LadderPlayer model here
-                        newLadder.AddRange(LadderPlayerList);
-                        if (newLadder.Count == result.Total || result.Entries.Count == 0)
-                        {
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        await _redisRepository.RemoveLeagueLadder(leagueName);
-                        break;
-                    }
-                }
-            }
+        //    var pages = Enumerable.Range(0, 25);
+        //    using (var rateGate = new RateGate(2, TimeSpan.FromSeconds(1)))
+        //    {
+        //        foreach (int page in pages)
+        //        {
+        //            await rateGate.WaitToProceed();
+        //            LadderApiResponse result = await FetchLadderApiPage(leagueName, page);
+        //            if (result != null)
+        //            {
+        //                var LadderPlayerList = result.Entries.Select(t => new LadderPlayerModel()
+        //                {
+        //                    Name = t.Character.Name,
+        //                    Level = t.Character.Level,
+        //                    Online = t.Online,
+        //                    Dead = t.Dead,
+        //                    Account = t.Account.Name,
+        //                    Experience = t.Character.Experience,
+        //                    ExperiencePerHour = 0,
+        //                    Rank = new LadderPlayerRankModel()
+        //                    {
+        //                        Overall = t.Rank
+        //                    },
+        //                    Depth = new LadderPlayerDepthModel()
+        //                    {
+        //                        Solo = t.Character.Depth != null ? t.Character.Depth.Solo : 0,
+        //                        Group = t.Character.Depth != null ? t.Character.Depth.@default : 0
+        //                    },
+        //                    Twitch = t.Account.Twitch?.Name,
+        //                    Class = t.Character.Class,
+        //                    Updated = DateTime.Now
+        //                }).ToList();
+        //                // Convert result to LadderPlayer model here
+        //                newLadder.AddRange(LadderPlayerList);
+        //                if (newLadder.Count == result.Total || result.Entries.Count == 0)
+        //                {
+        //                    break;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                await _redisRepository.RemoveLeagueLadder(leagueName);
+        //                break;
+        //            }
+        //        }
+        //    }
 
-            if (newLadder.Count > 0)
-            {
-                newLadder = CalculateStatistics(oldLadder, newLadder);
-                await _redisRepository.UpdateLeagueLadder(leagueName, newLadder);
-            }
-        }
+        //    if (newLadder.Count > 0)
+        //    {
+        //        newLadder = CalculateStatistics(oldLadder, newLadder);
+        //        await _redisRepository.UpdateLeagueLadder(leagueName, newLadder);
+        //    }
+        //}
 
-        private List<LadderPlayerModel> CalculateStatistics(List<LadderPlayerModel> oldLadder, List<LadderPlayerModel> newLadder)
-        {
-            foreach (var newEntry in newLadder)
-            {
-                newEntry.Depth.Group = newLadder.Count(t => t.Depth.Group > newEntry.Depth.Group) + 1;
-                newEntry.Depth.Solo = newLadder.Count(t => t.Depth.Solo > newEntry.Depth.Solo) + 1;
-                newEntry.Rank.Class = newLadder.Where(t => t.Class == newEntry.Class).Where(x => x.Rank.Overall < newEntry.Rank.Overall).Count() + 1;
+        //private List<LadderPlayerModel> CalculateStatistics(List<LadderPlayerModel> oldLadder, List<LadderPlayerModel> newLadder)
+        //{
+        //    foreach (var newEntry in newLadder)
+        //    {
+        //        newEntry.Depth.Group = newLadder.Count(t => t.Depth.Group > newEntry.Depth.Group) + 1;
+        //        newEntry.Depth.Solo = newLadder.Count(t => t.Depth.Solo > newEntry.Depth.Solo) + 1;
+        //        newEntry.Rank.Class = newLadder.Where(t => t.Class == newEntry.Class).Where(x => x.Rank.Overall < newEntry.Rank.Overall).Count() + 1;
 
-                if (oldLadder != null)
-                {
-                    var oldLadderEntry = oldLadder.FirstOrDefault(t => t.Name == newEntry.Name);
-                    if (oldLadderEntry != null && oldLadderEntry.Updated != DateTime.MinValue)
-                    {
-                        var expGain = newEntry.Experience - oldLadderEntry.Experience;
-                        var oneHour = (1 * 60 * 60);
-                        var timeBetweenUpdates = newEntry.Updated.ToUnixTimeStamp() - oldLadderEntry.Updated.ToUnixTimeStamp();
-                        var gainOverTime = (oneHour / timeBetweenUpdates) * expGain;
-                        newEntry.ExperiencePerHour = (long)gainOverTime;
-                    }
-                }
-            }
-            return newLadder;
-        }
+        //        if (oldLadder != null)
+        //        {
+        //            var oldLadderEntry = oldLadder.FirstOrDefault(t => t.Name == newEntry.Name);
+        //            if (oldLadderEntry != null && oldLadderEntry.Updated != DateTime.MinValue)
+        //            {
+        //                var expGain = newEntry.Experience - oldLadderEntry.Experience;
+        //                var oneHour = (1 * 60 * 60);
+        //                var timeBetweenUpdates = newEntry.Updated.ToUnixTimeStamp() - oldLadderEntry.Updated.ToUnixTimeStamp();
+        //                var gainOverTime = (oneHour / timeBetweenUpdates) * expGain;
+        //                newEntry.ExperiencePerHour = (long)gainOverTime;
+        //            }
+        //        }
+        //    }
+        //    return newLadder;
+        //}
 
-        public async Task<LadderApiResponse> FetchLadderApiPage(string league, int page)
-        {
-            var offset = page * 200;
-            league = HttpUtility.UrlEncode(league);
-            var urlParams = $"offset={offset}&limit=200&id={league}&type=league";
-            var url = $"{LadderUrl}?{urlParams}";
-            var apiResponse = await HandleLadderRequest(url);
-            return apiResponse;
-        }
+        //public async Task<LadderApiResponse> FetchLadderApiPage(string league, int page)
+        //{
+        //    var offset = page * 200;
+        //    league = HttpUtility.UrlEncode(league);
+        //    var urlParams = $"offset={offset}&limit=200&id={league}&type=league";
+        //    var url = $"{LadderUrl}?{urlParams}";
+        //    var apiResponse = await HandleLadderRequest(url);
+        //    return apiResponse;
+        //}
 
-        private async Task<LadderApiResponse> HandleLadderRequest(string url)
-        {
-            string json = await _externalService.ExecuteGetAsync(url);
-            if (json != null)
-            {
-                return JsonConvert.DeserializeObject<LadderApiResponse>(json);
-            }
-            return null;
-        }
+        //private async Task<LadderApiResponse> HandleLadderRequest(string url)
+        //{
+        //    string json = await _externalService.ExecuteGetAsync(url);
+        //    if (json != null)
+        //    {
+        //        return JsonConvert.DeserializeObject<LadderApiResponse>(json);
+        //    }
+        //    return null;
+        //}
     }
 }
