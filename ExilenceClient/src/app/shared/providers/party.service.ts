88a2e2f1cb63c0bd695130a9e912d00b3cee0c58
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-02-11 00:53:23 +0100

    Additional null-check for spectators

--- ExilenceClient/src/app/shared/providers/party.service.ts
+++ ExilenceClient/src/app/shared/providers/party.service.ts
@@ -164,7 +164,7 @@ export class PartyService implements OnDestroy {
           const spectators = this.updateSpectatorCount(this.party.players);
           this.stateService.dispatch({ key: 'spectatorCount', value: spectators });
 
-          if (this.playerLadders.find(x => player.character !== null && x.name === player.character.league) === undefined) {
+          if (this.playerLadders.find(x => x.name === player.character.league) === undefined && player.character !== null) {
             this.getLadderForLeague(player.character.league);
           }
 
@@ -214,7 +214,7 @@ export class PartyService implements OnDestroy {
         const spectators = this.updateSpectatorCount(this.party.players);
         this.stateService.dispatch({ key: 'spectatorCount', value: spectators });
 
-        if (this.playerLadders.find(x => player.character !== null && x.name === player.character.league) === undefined &&
+        if (this.playerLadders.find(x => x.name === player.character.league) === undefined &&
           player.character !== null) {
           this.getLadderForLeague(player.character.league);
         }
