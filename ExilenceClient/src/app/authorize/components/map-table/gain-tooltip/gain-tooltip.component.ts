0c478993b50b6f1bd2f5e55637daed0e23a51acf
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-02-15 11:16:53 +0100

    Static positioning of gain-tooltip

--- ExilenceClient/src/app/authorize/components/map-table/gain-tooltip/gain-tooltip.component.ts
+++ ExilenceClient/src/app/authorize/components/map-table/gain-tooltip/gain-tooltip.component.ts
@@ -32,24 +32,10 @@ export class GainTooltipComponent implements OnInit {
       const overflowRight = window.innerWidth - rect.right;
       const overflowBottom = window.innerHeight - rect.bottom;
 
-      // todo: set dynamically (see item-tooltip)
-      this.left = elementRect.left - 850;
-      this.top = elementRect.top;
+      this.left = 50;
+      this.top = 275;
 
       this.repositioned = true;
-
-      // if (overflowTop < 0) {
-      //   this.top = this.el.nativeElement.offsetTop - overflowTop;
-      // }
-      // if (overflowLeft < 0) {
-      //   this.left = this.el.nativeElement.offsetLeft - overflowLeft;
-      // }
-      // if (overflowBottom < 0) {
-      //   this.top = this.el.nativeElement.offsetTop + overflowBottom - margin;
-      // }
-      // if (overflowRight < 0) {
-      //   this.left = this.el.nativeElement.offsetLeft + overflowRight - margin;
-      // }
     }, 1);
 
   }
