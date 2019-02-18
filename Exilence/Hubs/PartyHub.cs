edb6dd233d924a334efbc6239af1ac889dc24923
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-02-18 16:22:03 +0100

    Fix for not updating proper player when joining on same connection with different characters

--- Exilence/Hubs/PartyHub.cs
+++ Exilence/Hubs/PartyHub.cs
@@ -99,9 +99,7 @@ namespace Exilence.Hubs
                 }
                 else
                 {
-                    var oldPlayer = party.Players.FirstOrDefault(x =>
-                        (x.Character != null && player.Character != null && x.Character.Name == player.Character.Name) ||
-                        x.ConnectionID == player.ConnectionID);
+                    var oldPlayer = party.Players.FirstOrDefault(x => x.ConnectionID == player.ConnectionID);
 
                     // if the party were joining doesnt have a leader, make the player leader
                     if (!party.Players.Any(x => x.IsLeader))
