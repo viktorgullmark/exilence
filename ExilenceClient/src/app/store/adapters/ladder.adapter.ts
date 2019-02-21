477b39f7bf7c0f0b8577d1ad5b32cd8933ea559c
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-02-21 16:29:35 +0100

    Fixed id-selection for ladder

--- ExilenceClient/src/app/store/adapters/ladder.adapter.ts
+++ ExilenceClient/src/app/store/adapters/ladder.adapter.ts
@@ -2,6 +2,7 @@ import { createEntityAdapter, EntityAdapter } from '@ngrx/entity';
 import { PlayerLadder } from '../../shared/interfaces/player.interface';
 
 export const adapter: EntityAdapter<PlayerLadder> = createEntityAdapter<PlayerLadder>({
+    selectId: (ladder: PlayerLadder) => ladder.name,
 });
 
 export const {
